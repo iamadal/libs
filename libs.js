7e8d2540cd9ff9545b2ca90333451bb3a2e1528e
iamadal <mdadalkhan@gmail.com>
2024-11-22 18:08:48 +0600

    edited libs.js

--- libs.js
+++ libs.js
@@ -1,11 +1,1 @@
-// api/index.js
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
-});
-
-// Export the express app wrapped as a serverless function
-module.exports.handler = serverless(app);
+// This is a test
